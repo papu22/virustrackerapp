c8a4bf657684f2cc217f897c42a76de3642bbc31
Biswajit Panda <pandabiswajit05@gmail.com>
2020-04-22 03:19:11 +0530

    Country name change

--- fetchdatebased.py
+++ fetchdatebased.py
@@ -7,6 +7,8 @@ from pytz import timezone
 import json
 
 world_data_json_url = "https://pomber.github.io/covid19/timeseries.json"
+country_list = {"US":"USA","Angola":"Anguilla","Congo (Kinshasa)":"Congo","Cote d'Ivoire":"Côte d'Ivoire","Holy See":"Holy See (Vatican City State)",
+                "United Kingdom":"UK","United Arab Emirates":"UAE","Syria":"Syrian Arab Republic","Taiwan*":"Taiwan","Korea, South":"S. Korea"}
 
 def divide_data_into_weeks(dict_with_date,sortorder):
     week = 0
@@ -42,8 +44,13 @@ def world_weekly_data(weekly_ind_data):
     request_data = requests.get(world_data_json_url)
     world_countries_data = request_data.json()
     world_weekly_data = {}
+    country_name = None
     world_weekly_data["India"] = weekly_ind_data
     for index,country in enumerate(world_countries_data):
+        if country in country_list.keys():
+            country_name = country_list[country]
+        else:
+            country_name = country
         if country != "India":
             weekly_dict_data = {}
             index = 0
@@ -70,10 +77,21 @@ def world_weekly_data(weekly_ind_data):
                 week_data.reverse()
                 weekly_dict_data[week] = {"dailydata":week_data}
             
-            world_weekly_data[country] = weekly_dict_data
+            world_weekly_data[country_name] = weekly_dict_data
     return  world_weekly_data
                 
-     
+
+
+
+def collect_world_graph_data(world_weekly_data):
+    world_data_graph = {}
+    for country in world_weekly_data.keys():
+        for week in world_weekly_data[country].keys():
+            if week not in world_data_graph.keys():
+                world_data_graph[week] = world_weekly_data[country][week]
+            else:
+                pass
+
 
 
 def collect_week_in_list(dict_with_date):
