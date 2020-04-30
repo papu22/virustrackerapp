2552210e546205490a6ea2546bf7cbfeef149037
Biswajit Panda <biswajit.panda@metlife.com>
2020-05-01 01:14:12 +0530

    Modifying logic

--- getWorldData.py
+++ getWorldData.py
@@ -19,8 +19,8 @@ world_state_data_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/C
 world_state_data_recovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 world_state_data_death = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 us_daily_data = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
-us_state_data_url = "https://covidtracking.com/api/states"
-us_state_daily_data_url = "https://covidtracking.com/api/states/daily"
+us_state_data_url = "https://covidtracking.com/api/states.json"
+us_state_daily_data_url = "https://covidtracking.com/api/states/daily.json"
 us_states = {'AK': 'Alaska', 'AL': 'Alabama', 'AR': 'Arkansas', 'AS': 'American Samoa', 'AZ': 'Arizona', 'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 'DC': 'District Of Columbia', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia', 'GU': 'Guam', 'HI': 'Hawaii', 'IA': 'Iowa', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'MA': 'Massachusetts', 'MD': 'Maryland', 'ME': 'Maine', 'MI': 'Michigan', 'MN': 'Minnesota', 'MO': 'Missouri', 'MP': 'Northern Mariana Islands', 'MS': 'Mississippi', 'MT': 'Montana', 'NC': 'North Carolina', 'ND': 'North Dakota', 'NE': 'Nebraska', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NV': 'Nevada', 'NY': 'New York', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'PR': 'Puerto Rico', 'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VA': 'Virginia', 'VI': 'US Virgin Islands', 'VT': 'Vermont', 'WA': 'Washington', 'WI': 'Wisconsin', 'WV': 'West Virginia', 'WY': 'Wyoming'}
 
 def collect_world_covid_19_data(total_world_data):
@@ -268,12 +268,7 @@ def get_all_us_state_data(seggregate_data_dict):
 
 
 
-# =============================================================================
-# def test_csv():
-#     test_data = pandas.read_csv("https://covidtracking.com/api/states.csv")
-#     print(test_data)
-# =============================================================================
-    
+
     
     
 def isNan(data):
@@ -282,7 +277,7 @@ def isNan(data):
 
 
 
-#test_csv()    
+
         
 #get_all_us_state_data()
 
